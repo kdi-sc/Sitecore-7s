e5cb5a19bf7a2840cb3ccaa30bd42c80cfadbed2
Kev <kdi@sitecore.net>
2022-09-29 20:49:30 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -212,7 +212,7 @@ const Home: NextPage<SevensProps> = (props): ReactElement<any> => {
                     "/" +
                     sevensItem.relativeUrl +
                     "?" +
-                    sevensItem.versionHash + "#t=20"
+                    sevensItem.versionHash + "#t=100"
                   }
                   onClick={() => {
                     handleOpen();
